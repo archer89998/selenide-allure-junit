c5426918822bde55c7dc17116fbe14a18cd3e86b
archer899998 <123456tbltns!>
2024-03-06 20:30:41 +0200

    Added parallel execution

---
+++ src/test/java/Google3Test.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+class Google3Test {
+
+    @BeforeEach
+    void setupAllure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @Test
+    void searchInGoogle() {
+        open("https://google.com");
+        $x(".//div[@Class='121']").should(Condition.visible);
+    }
+}
